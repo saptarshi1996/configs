0714f60e328b29c8824845fd59363bad56289f07
saptarshi1996 <saptarshisg96@gmail.com>
2025-05-06 14:40:17 +0530

    add mesloLGM

--- zshrc.py
+++ zshrc.py
@@ -141,7 +141,7 @@ omz_only = """
 export ZSH="$HOME/.oh-my-zsh"
 
 # Zsh theme (can be changed to your preference)
-ZSH_THEME="robbyrussell"
+ZSH_THEME="agnoster"
 
 # Load Oh My Zsh
 source $ZSH/oh-my-zsh.sh
