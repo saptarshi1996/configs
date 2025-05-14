879c0e625d954dff722c396db772ffb39f5c9402
saptarshi1996 <saptarshisg96@gmail.com>
2025-05-14 11:37:36 +0530

    change theme to robby

--- zshrc.py
+++ zshrc.py
@@ -142,7 +142,7 @@ omz_only = """
 export ZSH="$HOME/.oh-my-zsh"
 
 # Zsh theme (can be changed to your preference)
-ZSH_THEME="simple"
+ZSH_THEME="robbyrussell"
 
 # Load Oh My Zsh
 source $ZSH/oh-my-zsh.sh
